ea64db0b484daafefad6eb804e49038787e4835f
PacoLuna-AT <francisco.luna@agilethought.com>
2020-10-23 17:26:13 -0500

    Add encode password comparation test
    - Extract and compare data from request and mongo
    - Add feature scenario
    - Add stepdefinition for the comparation

--- src/test/java/com/globalClasses/MongoDBConnection.java
+++ src/test/java/com/globalClasses/MongoDBConnection.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
+import com.google.gson.JsonObject;
 import org.bson.Document;
 import org.bson.types.ObjectId;
 
@@ -28,104 +29,101 @@ import com.mongodb.client.model.Filters;
 
 public class MongoDBConnection {
 	private static MongoClient mClient;
-    private static MongoDatabase mDataBase;
+  private static MongoDatabase mDataBase;
 
 
-    public MongoDBConnection(String env, String db) {
-        Properties prop = new Properties();
-        String propFileName = "config.properties";
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
-        
-        try {
-            if(inputStream != null) {
-                prop.load(inputStream);
-            } else {
-                throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
-            }
-            String uriString = prop.getProperty(env + "." + db);
-            getMongoClient(uriString);
-            mDataBase = getDB(db);
-            System.out.println("Connection successful");
-        } catch(Exception e) {
-            e.printStackTrace();
-            System.out.println("Failed to make connection!");
-        }
+  public MongoDBConnection(String env, String db) {
+      Properties prop = new Properties();
+      String propFileName = "config.properties";
+      InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+      
+      try {
+          if(inputStream != null) {
+              prop.load(inputStream);
+          } else {
+              throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+          }
+          String uriString = prop.getProperty(env + "." + db);
+          getMongoClient(uriString);
+          mDataBase = getDB(db);
+          System.out.println("Connection successful");
+      } catch(Exception e) {
+          e.printStackTrace();
+          System.out.println("Failed to make connection!");
+      }
+  }
+
+  public void close() {
+      try {
+          if(mClient != null) {
+              mClient.close();
+              mClient = null;
+          }
+      } catch(Exception e) {
+          e.getMessage();
+      }
+  }
+
+  private MongoClient getMongoClient(String uriString) {
+      if(mClient == null) {
+          mClient = new MongoClient(new MongoClientURI(uriString));
+      }
+      return mClient;
+  }
+
+  private MongoDatabase getDB(String db) {
+      return mClient.getDatabase(db);
+  }
+
+  public boolean compareAllJsonString(String collection, String object) {
+
+      boolean bool = false;
+      List<String> mongoJson = new ArrayList<String>();
+
+      int i = 0;
+      MongoCollection<Document> coll = mDataBase.getCollection(collection);
+      FindIterable<Document> findIterable = coll.find();
+
+      for (Document document : findIterable) {
+          mongoJson.add(document.toJson());
+      }
+
+      bool = compareAllJS(mongoJson.toString(),object);
+
+      return bool;
+  }
+
+  public boolean compareAllJS(String mongoJson, String object) {
+
+    boolean bool = false;
+
+
+    JSONArray mongoJs = new JSONArray(mongoJson);
+
+    for (int i=0; mongoJs.length() > i; i++){
+        JSONObject jsnObject = mongoJs.getJSONObject(i);
+        jsnObject.remove("_id");
+        jsnObject.remove("password");
+
     }
 
-    public void close() {
-        try {
-            if(mClient != null) {
-                mClient.close();
-                mClient = null;
-            }
-        } catch(Exception e) {
-            e.getMessage();
-        }
+    JSONArray obj = new JSONArray(object);
+
+    for (int i=0; obj.length() > i; i++){
+        JSONObject jsnObject = obj.getJSONObject(i);
+        jsnObject.remove("id");
+        jsnObject.remove("password");
+
     }
 
-    private MongoClient getMongoClient(String uriString) {
-        if(mClient == null) {
-            mClient = new MongoClient(new MongoClientURI(uriString));
-        }
-        return mClient;
+    if(mongoJs.toString().equals(obj.toString())) {
+        bool = true;
     }
 
-    private MongoDatabase getDB(String db) {
-        return mClient.getDatabase(db);
-    }
-
-    public boolean compareAllJsonString(String collection, String object) {
-
-        boolean bool = false;
-        List<String> mongoJson = new ArrayList<String>();
-
-        int i = 0;
-        MongoCollection<Document> coll = mDataBase.getCollection(collection);
-        FindIterable<Document> findIterable = coll.find();
-
-        for (Document document : findIterable) {
-            mongoJson.add(document.toJson());
-        }
-
-        bool = compareAllJS(mongoJson.toString(),object);
-
-        return bool;
-    }
-
-    public boolean compareAllJS(String mongoJson, String object) {
-
-        boolean bool = false;
-
-
-        JSONArray mongoJs = new JSONArray(mongoJson);
-
-        for (int i=0; mongoJs.length() > i; i++){
-            JSONObject jsnObject = mongoJs.getJSONObject(i);
-            jsnObject.remove("_id");
-            jsnObject.remove("password");
-
-        }
-
-        JSONArray obj = new JSONArray(object);
-
-        for (int i=0; obj.length() > i; i++){
-            JSONObject jsnObject = obj.getJSONObject(i);
-            jsnObject.remove("id");
-            jsnObject.remove("password");
-
-        }
-
-        if(mongoJs.toString().equals(obj.toString())) {
-            //System.out.println("Equals");
-            bool = true;
-        }else {
-            //System.out.println("Non equals  =(");
-        }
-
-        return bool;
-    }
+    return bool;
+  }
     
-    public JSONArray executeQuerySelectID(String collection, String field, String filter) {
+  public JSONArray executeQuerySelectID(String collection, String field, String filter) {
 		JSONObject json = new JSONObject();
 		JSONArray resultJ = new JSONArray();
 		MongoCollection<Document> coll = mDataBase.getCollection(collection);
@@ -138,8 +136,8 @@ public class MongoDBConnection {
 		return resultJ;
 	}
     
-    public JSONArray obtainAll(String collection) {
-    	JSONObject json = new JSONObject();
+  public JSONArray obtainAll(String collection) {
+    JSONObject json = new JSONObject();
 		JSONArray resultJ = new JSONArray();
 		MongoCollection<Document> coll = mDataBase.getCollection(collection);
 		AggregateIterable<Document> output = coll.aggregate(Arrays.asList(Aggregates.sample(1)));
@@ -150,4 +148,47 @@ public class MongoDBConnection {
 		}
 		return resultJ;
     }
+
+    // P44
+  public boolean compareEncodedPassword(String collection, String id, String object) {
+
+    boolean bool = false;
+
+    JSONObject response_id = new JSONObject(id);
+    MongoCollection<Document> coll = mDataBase.getCollection(collection);
+    FindIterable<Document> findIterable = coll.find(Filters.eq("_id", new ObjectId(response_id.getString("id"))));
+
+    for (Document document : findIterable) {
+        JSONObject mongoJson = new JSONObject(document.toJson());
+        bool = comparePass(mongoJson,object );
+    }
+    return bool;
+  }
+  
+  public boolean comparePass(JSONObject mongoJson, String object) {
+
+    boolean bool = false;
+
+    mongoJson.remove("_id");
+    mongoJson.remove("name");
+    mongoJson.remove("firstName");
+    mongoJson.remove("lastName");
+    mongoJson.remove("email");
+    mongoJson.remove("status");
+
+    JSONObject obj = new JSONObject(object);
+
+    obj.remove("id");
+    obj.remove("name");
+    obj.remove("firstName");
+    obj.remove("lastName");
+    obj.remove("email");
+    obj.remove("status");
+
+    if(!mongoJson.toString().equals(obj.toString())) {
+        bool = true;
+    }
+  
+    return bool;
+  }
 }
--- src/test/java/com/globalClasses/MongoDBUtils.java
+++ src/test/java/com/globalClasses/MongoDBUtils.java
@@ -1,6 +1,8 @@
 package com.globalClasses;
 
+import com.google.gson.JsonObject;
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MongoDBUtils {
 	public static boolean compareAllJsonString(String env, String mDataBase, String collection, String json) {
@@ -33,6 +35,20 @@ public class MongoDBUtils {
 		db.close();
 		System.out.println("return result");
 		return querySelect;
+	}
+
+	public static boolean compareEncodedPassword(String env, String mDataBase, String collection, String id, String json) {
+		MongoDBConnection db = new MongoDBConnection(env, mDataBase);
+		boolean bool;
+		try {
+			bool = db.compareEncodedPassword(collection, id, json);
+		} catch(Exception var9) {
+			var9.printStackTrace();
+			bool = false;
+		} finally {
+			db.close();
+		}
+		return bool;
 	}
 }
 
